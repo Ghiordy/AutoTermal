d2964dd3993236b7068a6384b66135da0e2ba9af
Ghiordy <cghiordy@gmail.com>
2019-05-20 00:51:44 -0500

    adding mosquite receipter

--- mosquitto_rx.py
+++ mosquitto_rx.py
@@ -1,6 +1,6 @@
 #import paho.mqtt.client as paho
 import paho.mqtt.client as mqtt
-broker="192.168.1.86"
+broker="192.168.10.34"
 port=1883
 client = mqtt.Client()
 #def on_publish(client,userdata,result):             #create function for callback
@@ -12,7 +12,8 @@ client.connect(broker,port)                                 #establish connectio
 #ret= client1.publish("house/bulb1","on")                   #publish
 client.subscribe('#')
 def on_message( client, userdata, msg):
-    print(msg.payload)
+    print("Ajuste: ",float(msg.payload)," Celsius.")
+    print("Esperando nuevo valor tipo string2float.")
 pass
 client.on_message = on_message
 client.loop_forever()
